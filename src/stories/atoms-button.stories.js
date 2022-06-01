fb1773e6179a8f07191b0bddf46b411f8591916f
AiriIwatate <airiiwtt@icloud.com>
2022-06-01 19:06:35 +0900

    first

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,6 @@
+import Atoms_button from "../components/atoms/button/button.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "Atoms/Button" };
+
+export const Button = () => Atoms_button;
